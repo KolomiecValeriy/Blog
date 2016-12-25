99b08d69f5f617b4b751a09c18fd54c2add7e5f6
Valeriy Kolomiets <vel202007@gmail.com>
2016-12-25 22:54:02 +0200

    use add, edit, remove posts

--- src/Kolomiets/BlogBundle/Controller/DefaultController.php
+++ src/Kolomiets/BlogBundle/Controller/DefaultController.php
@@ -2,28 +2,123 @@
 
 namespace Kolomiets\BlogBundle\Controller;
 
+use Kolomiets\BlogBundle\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('KolomietsBlogBundle:Default:index.html.twig');
-    }
-
     /**
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    //KolomietsBlogBundle:Post
     public function showPostsAction()
     {
         $em = $this->getDoctrine()->getManager();
         $posts = $em->getRepository('KolomietsBlogBundle:Post')->findAll();
+        $removeLink =$this->generateUrl('removePost', [], UrlGeneratorInterface::ABSOLUTE_URL);
+        $editLink =$this->generateUrl('editPost', [], UrlGeneratorInterface::ABSOLUTE_URL);
 
-        return $this->render('KolomietsBlogBundle:Show:showPosts.html.twig',
-                [
-                    'posts' => $posts,
-                ]
-            );
+        return $this->render('KolomietsBlogBundle:Default:showPosts.html.twig',
+            [
+                'posts' => $posts,
+                'remove' => $removeLink,
+                'edit' => $editLink,
+            ]
+        );
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function addPostAction(Request $request)
+    {
+        $post = new Post();
+        $post->setName('Third name');
+        $post->setText('This is third text in this posts.');
+        $post->setDate(new \DateTime());
+        $post->setAuthor(2);
+
+        $form = $this->createFormBuilder($post)
+            ->add('name', TextType::class)
+            ->add('text', TextareaType::class)
+            ->add('date', DateType::class)
+            ->add('author', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Send'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirectToRoute('show_posts');
+        }
+
+        return $this->render('KolomietsBlogBundle:Default:form.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
+    }
+
+    /**
+     * @param int $postId
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function editPostAction($postId, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository('KolomietsBlogBundle:Post')->find($postId);
+
+        $form = $this->createFormBuilder($post)
+            ->add('name', TextType::class)
+            ->add('text', TextareaType::class)
+            ->add('date', DateType::class)
+            ->add('author', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Send'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirectToRoute('show_posts');
+        }
+
+        return $this->render('KolomietsBlogBundle:Default:form.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
+    }
+
+    /**
+     * @param int $postId
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function removePostAction($postId)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository('KolomietsBlogBundle:Post')->find($postId);
+        $em->remove($post);
+        $em->flush();
+
+        return $this->redirectToRoute('show_posts');
     }
 }
--- src/Kolomiets/BlogBundle/Repository/PostRepository.php
+++ src/Kolomiets/BlogBundle/Repository/PostRepository.php
@@ -11,7 +11,5 @@ use Doctrine\ORM\EntityRepository;
  */
 class PostRepository extends EntityRepository
 {
-    //insert INTO post (name, text, date, author) VALUES ("Some post","Some default text in this post", "2016-12-24", 1);
-    //insert INTO post (name, text, date, author) VALUES ("Second post","Some second text in this post", "2016-12-25", 2);
 
 }
