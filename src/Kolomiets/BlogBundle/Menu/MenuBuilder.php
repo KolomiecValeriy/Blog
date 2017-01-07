d7f32af512842c95cc6063a151499ce0f651935c
Valeriy Kolomiets <vel202007@gmail.com>
2017-01-07 23:02:20 +0200

    Added main menu

---
+++ src/Kolomiets/BlogBundle/Menu/MenuBuilder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Kolomiets\BlogBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+
+class MenuBuilder
+{
+    private $factory;
+
+    /**
+     * @param FactoryInterface $factory
+     *
+     * Add any other dependency you need
+     */
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function createMainMenu(array $options)
+    {
+        $menu = $this->factory->createItem('mainMenu');
+
+        $menu->addChild('All posts', array('route' => 'show_posts'));
+        $menu->addChild('Add post', array('route' => 'add_post'));
+        $menu->setChildrenAttribute('class', 'nav navbar-nav');
+        // ... add more children
+
+        return $menu;
+    }
+}
