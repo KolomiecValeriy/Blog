97e46c3be23118064530e1445949f99cd67c1ecc
Valeriy Kolomiets <vel202007@gmail.com>
2017-01-15 17:47:43 +0200

    Added edit & remove form type

--- src/Kolomiets/BlogBundle/Controller/DefaultController.php
+++ src/Kolomiets/BlogBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Kolomiets\BlogBundle\Controller;
 
 use Kolomiets\BlogBundle\Entity\Post;
+use Kolomiets\BlogBundle\Form\Type\EditPostType;
 use Kolomiets\BlogBundle\Form\Type\PostType;
 use Kolomiets\BlogBundle\Form\Type\RemovePostType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -11,28 +12,48 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DefaultController extends Controller
 {
     /**
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function showPostsAction()
+    public function showPostsAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
         $posts = $em->getRepository('KolomietsBlogBundle:Post')->findAll();
         $categories = $em->getRepository('KolomietsBlogBundle:Category')->findAll();
 
-        $removeLink =$this->generateUrl('removePost', [], UrlGeneratorInterface::ABSOLUTE_URL);
-        $editLink =$this->generateUrl('editPost', [], UrlGeneratorInterface::ABSOLUTE_URL);
+        for($i = 0; $i < count($posts); $i++) {
+            $removeForm[$i] = $this->createForm(RemovePostType::class);
+            $editForm[$i] = $this->createForm(EditPostType::class);
+
+            if ($request->isMethod($request::METHOD_POST)) {
+                $removeForm[$i]->handleRequest($request);
+                $editForm[$i]->handleRequest($request);
+
+                if ($removeForm[$i]->isSubmitted() && $removeForm[$i]->isValid()) {
+                    $post = $removeForm[$i]->getData();
+
+                    return $this->redirectToRoute('remove_post', ['postId' => $post['name']]);
+                }
+
+                if ($editForm[$i]->isSubmitted() && $editForm[$i]->isValid()) {
+                    $post = $editForm[$i]->getData();
+
+                    return $this->redirectToRoute('edit_post', ['postId' => $post['name']]);
+                }
+            }
+            $removeForm[$i] = $removeForm[$i]->createView();
+            $editForm[$i] = $editForm[$i]->createView();
+        }
 
         return $this->render('KolomietsBlogBundle:Default:showPosts.html.twig',
             [
                 'posts' => $posts,
                 'categories' => $categories,
-                'remove' => $removeLink,
-                'edit' => $editLink,
+                'removeForm' => $removeForm,
+                'editForm' => $editForm,
             ]
         );
     }
@@ -44,7 +65,6 @@ class DefaultController extends Controller
     public function addPostAction(Request $request)
     {
         $post = new Post();
-
         $form = $this->createForm(PostType::class, $post);
 
         if($request->isMethod($request::METHOD_POST)) {
@@ -78,24 +98,19 @@ class DefaultController extends Controller
         $em = $this->getDoctrine()->getManager();
         $post = $em->getRepository('KolomietsBlogBundle:Post')->find($postId);
 
-        $form = $this->createFormBuilder($post)
-            ->add('name', TextType::class)
-            ->add('text', TextareaType::class)
-            ->add('date', DateType::class)
-            ->add('author', TextType::class)
-            ->add('save', SubmitType::class, ['label' => 'Send'])
-            ->getForm();
+        $form = $this->createForm(PostType::class, $post);
 
-        $form->handleRequest($request);
+        if($request->isMethod($request::METHOD_POST)) {
+            $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $post = $form->getData();
+            if ($form->isSubmitted() && $form->isValid()) {
+                $post = $form->getData();
 
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($post);
-            $em->flush();
+                $em = $this->getDoctrine()->getManager();
+                $em->flush();
 
-            return $this->redirectToRoute('show_posts');
+                return $this->redirectToRoute('show_posts');
+            }
         }
 
         return $this->render('KolomietsBlogBundle:Default:form.html.twig',
---
+++ src/Kolomiets/BlogBundle/Form/Type/RemovePostType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Kolomiets\BlogBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RemovePostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', HiddenType::class)
+            ->add('submit', SubmitType::class, ['label' => 'Remove']);
+    }
+
+    public function getName()
+    {
+        return 'kolomiets_blog_bundle_remove_post_type';
+    }
+}
